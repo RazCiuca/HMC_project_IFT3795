190829a15ddf06d0f1ecd56a4d6cba7a9f462271
terramorpha <terramorpha@cock.li>
2024-04-27 20:16:30 -0400

    generate 3d visualisation through Isomap

--- isomap.py
+++ isomap.py
@@ -10,12 +10,12 @@ def generate_figures():
     # cifar10_test_not_augmented = t.load('./datasets/cifar10_test_not_augmented.pth')
     # cifar10_test_augmented = t.load('./datasets/cifar10_test_augmented.pth')
 
-    things = [
+    sets = [
         ("cifar10_training", *cifar10_training),
         ("cifar10_training_augmented", *cifar10_training_augmented),
     ]
 
-    for title, x, y in things:
+    for title, x, y in sets:
         shape = x.shape
         reshaped = x.reshape(shape[0], np.prod(shape[1:]))
         N_samples = 4000
@@ -24,19 +24,42 @@ def generate_figures():
         subsampled_images = reshaped[samples, :]
         subsampled_labels = y[samples]
 
-        isomap = Isomap()
+        isomap = Isomap(n_components=3)
         new_coords = isomap.fit_transform(subsampled_images)
 
+        print(new_coords.shape)
         # On fabrique une grille
-        x0, x1 = min(new_coords[:, 0]), max(new_coords[:, 0])
-        y0, y1 = min(new_coords[:, 1]), max(new_coords[:, 1])
-        nxn = 10
-        xsize = (x1 - x0)/nxn
-        ysize = (y1 - y0)/nxn
-        xx, yy = np.meshgrid(np.linspace(x0, x1, nxn), np.linspace(y0, y1, nxn))
+        # x0, x1 = min(new_coords[:, 0]), max(new_coords[:, 0])
+        # y0, y1 = min(new_coords[:, 1]), max(new_coords[:, 1])
+        # nxn = 10
+        # xsize = (x1 - x0)/nxn
+        # ysize = (y1 - y0)/nxn
+        # xx, yy = np.meshgrid(np.linspace(x0, x1, nxn), np.linspace(y0, y1, nxn))
 
         # On crée la figure
-        plt.scatter(new_coords[:, 0], new_coords[:, 1], c=subsampled_labels, alpha=0.5)
+        fig = plt.figure()
+        ax = fig.add_subplot()
+        ax.scatter(new_coords[:, 0], new_coords[:, 1], c=subsampled_labels, alpha=0.5)
         plt.title("Plongement 2D des images par isomap")
-        plt.savefig(f"{title}.png", dpi=500)
-        plt.clf()
+        fig.savefig(f"{title}_2d.png", dpi=500)
+
+        fig = plt.figure()
+        ax = fig.add_subplot(projection='3d')
+        ax.scatter(new_coords[:, 0], new_coords[:, 1], new_coords[:, 2], c=subsampled_labels, alpha=0.5)
+        plt.title("Plongement 3D des images par isomap")
+        fig.savefig(f"{title}_3d.png", dpi=500)
+
+def test():
+    x, y = t.load('./datasets/cifar10_training.pth')
+
+    shape = x.shape
+    reshaped = x.reshape(shape[0], np.prod(shape[1:]))
+    N_samples = 4000
+    samples = np.random.choice(shape[0], N_samples)
+
+    subsampled_images = reshaped[samples, :]
+    subsampled_labels = y[samples]
+
+    isomap = Isomap(n_components=3)
+    new_coords = isomap.fit_transform(subsampled_images)
+    return new_coords
